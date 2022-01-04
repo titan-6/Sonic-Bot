0db7dde75920920dde028c3b224c1cc88dc523fd
Neil K <64322243+titan-6@users.noreply.github.com>
2022-01-04 17:23:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+client =  commands.Bot(command_prefix = "?", intents = 
+discord.Intents.all())
+
+for i in range(len(cogs)):
+  cogs[i].setup(client)
+
+
+my_secret = os.environ['key']
+client.run(my_secret)
